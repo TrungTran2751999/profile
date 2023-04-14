b0003f15593079bafd92db6452e76418469ead83
TrungTran2751999 <trantrung2751999@gmail.com>
2023-04-14 22:43:32 +0700

    profile

---
+++ assets/js/i18n.js
@@ -0,0 +1,24 @@
+let pathVi = "../assets/i18n/vi.json";
+function translate(path){
+    $.ajax({
+        url: path,
+        type: "GET",
+        async: false
+    })
+    .done((result)=>{
+        renderJson(result)
+    })
+}
+function renderJson(json){
+    let valueTotal = Object.values(json);
+    valueTotal.map(value=>{
+        let valueChildrenArr = Object.values(value);
+        let keyChildrenArr = Object.keys(value);
+        if(!keyChildrenArr.includes("prop")){
+            $(valueChildrenArr[0]).html(valueChildrenArr[1]);
+        }else{
+            $(valueChildrenArr[0]).attr(valueChildrenArr[2], valueChildrenArr[1]);
+        }
+    })
+}
+translate(pathVi);
